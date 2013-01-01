52283bcd7148990164ce7dada13e8a7850cc2549
Avi Romanoff <aviromanoff@gmail.com>
2013-01-01 00:24:40 -0500

    Drop deprecated code

--- server.js
+++ server.js
@@ -264,9 +264,6 @@
 
   ss.on("connection", function(err, socket, session) {
     var L, broadcast, saveSession, sync, token;
-    if (!session) {
-      return socket.disconnect();
-    }
     token = session.token;
     socket.join(token.username);
     saveSession = function(cb) {
